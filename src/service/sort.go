42a2ccb19cb5887c665ebba433e5098f616e9fec
Deyan Enchev <deyan.enchev@scalefocus.com>
2023-02-09 02:06:52 +0200

    Implementing cycle detection inside the sorting function

--- src/service/sort.go
+++ src/service/sort.go
@@ -2,44 +2,11 @@ package service
 
 import (
 	"errors"
-	"sort"
 
 	"github.com/deyanEnchev/src/model"
 )
 
-func SortTasks(job model.Job) ([]model.Task, error) {
-	isCycle := cycleDetector(job)
-	if isCycle {
-		return nil, errors.New("cycle detected")
-	}
-	taskIndices := make(map[string]int)
-	for i, task := range job.Tasks {
-		taskIndices[task.Name] = i
-	}
-	sort.SliceStable(job.Tasks, func(i, j int) bool {
-		a := job.Tasks[i]
-		b := job.Tasks[j]
-		for _, req := range a.Requires {
-			if taskIndices[req] > i {
-				return false
-			}
-		}
-		for _, req := range b.Requires {
-			if taskIndices[req] > j {
-				return true
-			}
-		}
-		return false
-	})
-
-	return job.Tasks, nil
-}
-
 func TopologicalSort(j model.Job) ([]model.Task, error) {
-	isCycle := cycleDetector(j)
-	if isCycle {
-		return nil, errors.New("cycle detected")
-	}
 	taskMap := make(map[string]model.Task)
 	for _, task := range j.Tasks {
 		taskMap[task.Name] = task
@@ -47,48 +14,38 @@ func TopologicalSort(j model.Job) ([]model.Task, error) {
 
 	var sortedTasks []model.Task
 	visited := make(map[string]bool)
+	recStack := make(map[string]bool)
 
-	var visit func(taskName string)
-	visit = func(taskName string) {
+	var visit func(taskName string) bool
+	visit = func(taskName string) bool {
+		if recStack[taskName] {
+			return true
+		}
+
 		if visited[taskName] {
-			return
+			return false
 		}
 		visited[taskName] = true
+		recStack[taskName] = true
 
 		task := taskMap[taskName]
 		for _, requirement := range task.Requires {
-			visit(requirement)
-		}
-
-		sortedTasks = append(sortedTasks, task)
-	}
-
-	for _, task := range j.Tasks {
-		visit(task.Name)
-	}
-
-	return sortedTasks, nil
-}
-
-func cycleDetector(job model.Job) bool {
-	for i, t1 := range job.Tasks {
-		for _, t2 := range job.Tasks[i+1:] {
-			isTaskOneRequired := false
-			isTaskTwoRequired := false
-			for _, req2 := range t2.Requires {
-				if t1.Name == req2 {
-					isTaskOneRequired = true
-				}
-			}
-			for _, req1 := range t1.Requires {
-				if t2.Name == req1 {
-					isTaskTwoRequired = true
-				}
-			}
-			if isTaskOneRequired && isTaskTwoRequired {
+			if visit(requirement) {
 				return true
 			}
 		}
+
+		sortedTasks = append(sortedTasks, task)
+
+		recStack[taskName] = false
+		return false
 	}
-	return false
+
+	for _, task := range j.Tasks {
+		if visit(task.Name) {
+			return nil, errors.New("cycle detected")
+		}
+	}
+
+	return sortedTasks, nil
 }
